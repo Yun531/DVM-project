f53e311f4976695b624991c46812835a34d69866
3daysJin-MAIN\Lee Seung Jin <3daysjin@gmail.com>
2022-05-26 09:50:16 +0900

    <fix> createVerificationCode() 대문자 제거

--- DVM/src/Controller.java
+++ DVM/src/Controller.java
@@ -300,7 +300,7 @@ public class Controller {
         Random random = new Random();
 
         String vCode = random.ints(leftLimit,rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .filter(i -> (i <= 57 || i >= 97))
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
